0f32552804e8bf957b15ae063ce58f7b0677db93
KDN <kdn@webaix.de>
2020-07-03 09:14:45 +0200

    first version

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-shopware-cms-helper
+<h1 align="center">Welcome to shopware-cms-helper 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+  <img src="https://img.shields.io/badge/npm-%3E%3D6.13.4-blue.svg" />
+  <img src="https://img.shields.io/badge/node-%3E%3D10.18.1-blue.svg" />
+  <a href="https://github.com/Peeet93/shopware-cms-helper#readme" target="_blank">
+    <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" />
+  </a>
+  <a href="https://github.com/Peeet93/shopware-cms-helper/graphs/commit-activity" target="_blank">
+    <img alt="Maintenance" src="https://img.shields.io/badge/Maintained%3F-yes-green.svg" />
+  </a>
+  <a href="https://github.com/Peeet93/shopware-cms-helper/blob/master/LICENSE" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/github/license/Peeet93/shopware-cms-helper" />
+  </a>
+</p>
+
+> Create Shopware cms element and block structure
+
+### 🏠 [Homepage](https://github.com/Peeet93/shopware-cms-helper#readme)
+
+## Prerequisites
+
+- npm >=6.13.4
+- node >=10.18.1
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+```sh
+npm run start
+```
+
+You can also create a shell script and place it in your path to use shopware-cms-helper everywhere.<br>
+The script should look like this:
+
+```sh
+node %your_path_to_repository%/index.js
+```
+
+## Author
+
+👤 **Kevin Dohlen**
+
+* Website: https://www.webaix.de
+* Github: [@Peeet93](https://github.com/Peeet93)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/Peeet93/shopware-cms-helper/issues). 
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+## 📝 License
+
+Copyright © 2020 [Kevin Dohlen](https://github.com/Peeet93).<br />
+This project is [MIT](https://github.com/Peeet93/shopware-cms-helper/blob/master/LICENSE) licensed.
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ index.js
@@ -0,0 +1,130 @@
+'use strict';
+const inquirer = require('inquirer');
+const currentDir = process.cwd();
+const fs = require('fs');
+
+let questions = [
+    {
+        type: 'list',
+        name: 'type',
+        message: "Block or Element?",
+        choices: ['Block', 'Element'],
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Choose your name:',
+        validate: function (input) {
+            return input !== "";
+        }
+    }
+];
+
+let createDirectory = function(dir){
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+        return true;
+    }
+    return false;
+}
+
+inquirer.prompt(questions).then((answers) => {
+    const swPrefix = "sw-cms-";
+
+    let elementDir = currentDir + "\\" + answers.name + "\\";
+
+    let preview = elementDir + "preview\\";
+    let config = elementDir + "config\\";
+    let component = elementDir + "component\\";
+
+    let block;
+    let templateFolder;
+    let previewClass;
+    let previewBlock;
+    let componentClass;
+    let componentBlock;
+    let configClass;
+    let configBlock;
+
+
+    if(!createDirectory(elementDir)){
+        console.log("Folder already exists!");
+        return;
+    }
+    createDirectory(preview);
+    createDirectory(component);
+
+    if(answers.type === "Block"){
+        templateFolder = `${__dirname}/templates/block/`;
+        previewClass = `${swPrefix}block-preview-${answers.name}`;
+        previewBlock = `${swPrefix.replace('-', '_')}block_preview_${answers.name.replace('-', '_')}`;
+
+        componentClass = `${swPrefix}block-${answers.name}`;
+        componentBlock = `${swPrefix.replace('-', '_')}block_${answers.name.replace('-', '_')}`;
+
+    }else{
+        templateFolder = `${__dirname}/templates/element/`;
+        previewClass = `${swPrefix}el-preview-${answers.name}`;
+        previewBlock = `${swPrefix.replace('-', '_')}element_preview_${answers.name.replace('-', '_')}`;
+
+        configClass = `${swPrefix}el-config-${answers.name}`
+        configBlock = `${swPrefix.replace('-', '_')}element_config_${answers.name.replace('-', '_')}`
+
+        componentClass = `${swPrefix}el-${answers.name}`;
+        componentBlock = `${swPrefix.replace('-', '_')}element_${answers.name.replace('-', '_')}`;
+
+        createDirectory(config);
+
+        /////////////////////// SCSS ////////////////////////
+        block = fs.readFileSync(`${templateFolder}config/config.scss.template`, "utf8");
+        block = block.replace(/##class##/g, configClass);
+        fs.writeFileSync(`${config}${configClass}.scss`, block);
+        /////////////////////// TWIG ////////////////////////
+        block = fs.readFileSync(`${templateFolder}config/config.html.twig.template`, "utf8");
+        block = block.replace(/##class##/g, configClass);
+        block = block.replace(/##block##/g, configBlock);
+        fs.writeFileSync(`${config}${configClass}.html.twig`, block);
+        /////////////////////// JS ////////////////////////
+        block = fs.readFileSync(`${templateFolder}config/index.js.template`, "utf8");
+        block = block.replace(/##class##/g, configClass);
+        block = block.replace(/##name##/g, answers.name);
+        fs.writeFileSync(`${config}index.js`, block);
+    }
+
+    /////////////////////// SCSS ////////////////////////
+    block = fs.readFileSync(`${templateFolder}preview/preview.scss.template`, "utf8");
+    block = block.replace(/##class##/g, previewClass);
+    fs.writeFileSync(`${preview}${previewClass}.scss`, block);
+
+    block = fs.readFileSync(`${templateFolder}component/component.scss.template`, "utf8");
+    block = block.replace(/##class##/g, componentClass);
+    fs.writeFileSync(`${component}${componentClass}.scss`, block);
+    /////////////////////// TWIG ////////////////////////
+    block = fs.readFileSync(`${templateFolder}preview/preview.html.twig.template`, "utf8");
+    block = block.replace(/##class##/g, previewClass);
+    block = block.replace(/##block##/g, previewBlock);
+    fs.writeFileSync(`${preview}${previewClass}.html.twig`, block);
+
+    block = fs.readFileSync(`${templateFolder}component/component.html.twig.template`, "utf8");
+    block = block.replace(/##class##/g, componentClass);
+    block = block.replace(/##block##/g, componentBlock);
+    fs.writeFileSync(`${component}${componentClass}.html.twig`, block);
+    /////////////////////// JS ////////////////////////
+    block = fs.readFileSync(`${templateFolder}preview/index.js.template`, "utf8");
+    block = block.replace(/##class##/g, previewClass);
+    fs.writeFileSync(`${preview}index.js`, block);
+
+    block = fs.readFileSync(`${templateFolder}component/index.js.template`, "utf8");
+    block = block.replace(/##class##/g, componentClass);
+    block = block.replace(/##name##/g, answers.name);
+    fs.writeFileSync(`${component}index.js`, block);
+    /////////////////////// index.js ////////////////////////
+    block = fs.readFileSync(`${templateFolder}index.js.template`, "utf8");
+    block = block.replace(/##name##/g, answers.name);
+    block = block.replace(/##preview-class##/g, previewClass);
+    if(configClass){
+        block = block.replace(/##config-class##/g, configClass);
+    }
+    block = block.replace(/##component-class##/g, componentClass);
+    fs.writeFileSync(`${elementDir}index.js`, block);
+});
